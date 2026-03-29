1507fd9f5613e560a7b6512ba7f9d0dd3bbfe305
yuriy [7505718860016828420] <>
2026-03-30 06:11:03 +0800

    auto save-2026-03-29 ext

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-lsof -nP -iTCP:18789 -sTCP:LISTEN -t | xargs -r kill -9; nohup openclaw gateway run --port 18789 > /tmp/openclaw-gateway.log 2>&1 &
+if [ -d "/run/systemd/system/" ]; then
+  lsof -nP -iTCP:18789 -sTCP:LISTEN -t | xargs -r kill -9; sudo systemctl restart openclaw
+else
+  lsof -nP -iTCP:18789 -sTCP:LISTEN -t | xargs -r kill -9; nohup openclaw gateway run --port 18789 > /tmp/openclaw-gateway.log 2>&1 &
+fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-nohup openclaw gateway run --port 18789 > /tmp/openclaw-gateway.log 2>&1 &
+if [ -d "/run/systemd/system/" ]; then
+    sudo systemctl start openclaw
+else
+    nohup openclaw gateway run --port 18789 > /tmp/openclaw-gateway.log 2>&1 &
+fi
