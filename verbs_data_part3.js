7377700c2ecbd2e20ac8b79a6529a32090a2ecc1
GNK48-套中人 <zhouyu00@users.noreply.github.com>
2026-04-10 18:39:06 +0800

    Add verbs_data_part3.js

---
+++ verbs_data_part3.js
@@ -0,0 +1,268 @@
+// 第二组 -IR 动词数据 (20个)
+const group2Verbs = [
+    {
+        infinitif: 'finir',
+        meaning: '完成，结束',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['finis', 'finis', 'finit', 'finissons', 'finissez', 'finissent'],
+            passe_compose: ['ai fini', 'as fini', 'a fini', 'avons fini', 'avez fini', 'ont fini'],
+            imparfait: ['finissais', 'finissais', 'finissait', 'finissions', 'finissiez', 'finissaient'],
+            futur_simple: ['finirai', 'finiras', 'finira', 'finirons', 'finirez', 'finiront'],
+            conditionnel: ['finirais', 'finirais', 'finirait', 'finirions', 'finiriez', 'finiraient'],
+            subjonctif: ['finisse', 'finisses', 'finisse', 'finissions', 'finissiez', 'finissent']
+        }
+    },
+    {
+        infinitif: 'choisir',
+        meaning: '选择',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['choisis', 'choisis', 'choisit', 'choisissons', 'choisissez', 'choisissent'],
+            passe_compose: ['ai choisi', 'as choisi', 'a choisi', 'avons choisi', 'avez choisi', 'ont choisi'],
+            imparfait: ['choisissais', 'choisissais', 'choisissait', 'choisissions', 'choisissiez', 'choisissaient'],
+            futur_simple: ['choisirai', 'choisiras', 'choisira', 'choisirons', 'choisirez', 'choisiront'],
+            conditionnel: ['choisirais', 'choisirais', 'choisirait', 'choisirions', 'choisiriez', 'choisiraient'],
+            subjonctif: ['choisisse', 'choisisses', 'choisisse', 'choisissions', 'choisissiez', 'choisissent']
+        }
+    },
+    {
+        infinitif: 'réussir',
+        meaning: '成功',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['réussis', 'réussis', 'réussit', 'réussissons', 'réussissez', 'réussissent'],
+            passe_compose: ['ai réussi', 'as réussi', 'a réussi', 'avons réussi', 'avez réussi', 'ont réussi'],
+            imparfait: ['réussissais', 'réussissais', 'réussissait', 'réussissions', 'réussissiez', 'réussissaient'],
+            futur_simple: ['réussirai', 'réussiras', 'réussira', 'réussirons', 'réussirez', 'réussiront'],
+            conditionnel: ['réussirais', 'réussirais', 'réussirait', 'réussirions', 'réussiriez', 'réussiraient'],
+            subjonctif: ['réussisse', 'réussisses', 'réussisse', 'réussissions', 'réussissiez', 'réussissent']
+        }
+    },
+    {
+        infinitif: 'grandir',
+        meaning: '成长，长大',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['grandis', 'grandis', 'grandit', 'grandissons', 'grandissez', 'grandissent'],
+            passe_compose: ['ai grandi', 'as grandi', 'a grandi', 'avons grandi', 'avez grandi', 'ont grandi'],
+            imparfait: ['grandissais', 'grandissais', 'grandissait', 'grandissions', 'grandissiez', 'grandissaient'],
+            futur_simple: ['grandirai', 'grandiras', 'grandira', 'grandirons', 'grandirez', 'grandiront'],
+            conditionnel: ['grandirais', 'grandirais', 'grandirait', 'grandirions', 'grandiriez', 'grandiraient'],
+            subjonctif: ['grandisse', 'grandisses', 'grandisse', 'grandissions', 'grandissiez', 'grandissent']
+        }
+    },
+    {
+        infinitif: 'grossir',
+        meaning: '变胖，增加',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['grossis', 'grossis', 'grossit', 'grossissons', 'grossissez', 'grossissent'],
+            passe_compose: ['ai grossi', 'as grossi', 'a grossi', 'avons grossi', 'avez grossi', 'ont grossi'],
+            imparfait: ['grossissais', 'grossissais', 'grossissait', 'grossissions', 'grossissiez', 'grossissaient'],
+            futur_simple: ['grossirai', 'grossiras', 'grossira', 'grossirons', 'grossirez', 'grossiront'],
+            conditionnel: ['grossirais', 'grossirais', 'grossirait', 'grossirions', 'grossiriez', 'grossiraient'],
+            subjonctif: ['grossisse', 'grossisses', 'grossisse', 'grossissions', 'grossissiez', 'grossissent']
+        }
+    },
+    {
+        infinitif: 'maigrir',
+        meaning: '变瘦',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['maigris', 'maigris', 'maigrit', 'maigrissons', 'maigrissez', 'maigrissent'],
+            passe_compose: ['ai maigri', 'as maigri', 'a maigri', 'avons maigri', 'avez maigri', 'ont maigri'],
+            imparfait: ['maigrissais', 'maigrissais', 'maigrissait', 'maigrissions', 'maigrissiez', 'maigrissaient'],
+            futur_simple: ['maigrirai', 'maigriras', 'maigrira', 'maigrirons', 'maigrirez', 'maigriront'],
+            conditionnel: ['maigrirais', 'maigrirais', 'maigrirait', 'maigririons', 'maigririez', 'maigriraient'],
+            subjonctif: ['maigrisse', 'maigrisses', 'maigrisse', 'maigrissions', 'maigrissiez', 'maigrissent']
+        }
+    },
+    {
+        infinitif: 'punir',
+        meaning: '惩罚',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['punis', 'punis', 'punit', 'punissons', 'punissez', 'punissent'],
+            passe_compose: ['ai puni', 'as puni', 'a puni', 'avons puni', 'avez puni', 'ont puni'],
+            imparfait: ['punissais', 'punissais', 'punissait', 'punissions', 'punissiez', 'punissaient'],
+            futur_simple: ['punirai', 'puniras', 'punira', 'punirons', 'punirez', 'puniront'],
+            conditionnel: ['punirais', 'punirais', 'punirait', 'punirions', 'puniriez', 'puniraient'],
+            subjonctif: ['punisse', 'punisses', 'punisse', 'punissions', 'punissiez', 'punissent']
+        }
+    },
+    {
+        infinitif: 'réfléchir',
+        meaning: '思考，考虑',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['réfléchis', 'réfléchis', 'réfléchit', 'réfléchissons', 'réfléchissez', 'réfléchissent'],
+            passe_compose: ['ai réfléchi', 'as réfléchi', 'a réfléchi', 'avons réfléchi', 'avez réfléchi', 'ont réfléchi'],
+            imparfait: ['réfléchissais', 'réfléchissais', 'réfléchissait', 'réfléchissions', 'réfléchissiez', 'réfléchissaient'],
+            futur_simple: ['réfléchirai', 'réfléchiras', 'réfléchira', 'réfléchirons', 'réfléchirez', 'réfléchiront'],
+            conditionnel: ['réfléchirais', 'réfléchirais', 'réfléchirait', 'réfléchirions', 'réfléchiriez', 'réfléchiraient'],
+            subjonctif: ['réfléchisse', 'réfléchisses', 'réfléchisse', 'réfléchissions', 'réfléchissiez', 'réfléchissent']
+        }
+    },
+    {
+        infinitif: 'obéir',
+        meaning: '服从',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['obéis', 'obéis', 'obéit', 'obéissons', 'obéissez', 'obéissent'],
+            passe_compose: ['ai obéi', 'as obéi', 'a obéi', 'avons obéi', 'avez obéi', 'ont obéi'],
+            imparfait: ['obéissais', 'obéissais', 'obéissait', 'obéissions', 'obéissiez', 'obéissaient'],
+            futur_simple: ['obéirai', 'obéiras', 'obéira', 'obéirons', 'obéirez', 'obéiront'],
+            conditionnel: ['obéirais', 'obéirais', 'obéirait', 'obéirions', 'obéiriez', 'obéiraient'],
+            subjonctif: ['obéisse', 'obéisses', 'obéisse', 'obéissions', 'obéissiez', 'obéissent']
+        }
+    },
+    {
+        infinitif: 'servir',
+        meaning: '服务，供应',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['sers', 'sers', 'sert', 'servons', 'servez', 'servent'],
+            passe_compose: ['ai servi', 'as servi', 'a servi', 'avons servi', 'avez servi', 'ont servi'],
+            imparfait: ['servais', 'servais', 'servait', 'servions', 'serviez', 'servaient'],
+            futur_simple: ['servirai', 'serviras', 'servira', 'servirons', 'servirez', 'serviront'],
+            conditionnel: ['servirais', 'servirais', 'servirait', 'servirions', 'serviriez', 'serviraient'],
+            subjonctif: ['serve', 'serves', 'serve', 'servions', 'serviez', 'servent']
+        }
+    },
+    {
+        infinitif: 'sentir',
+        meaning: '感觉，闻',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['sens', 'sens', 'sent', 'sentons', 'sentez', 'sentent'],
+            passe_compose: ['ai senti', 'as senti', 'a senti', 'avons senti', 'avez senti', 'ont senti'],
+            imparfait: ['sentais', 'sentais', 'sentait', 'sentions', 'sentiez', 'sentaient'],
+            futur_simple: ['sentirai', 'sentiras', 'sentira', 'sentirons', 'sentirez', 'sentiront'],
+            conditionnel: ['sentirais', 'sentirais', 'sentirait', 'sentirions', 'sentiriez', 'sentiraient'],
+            subjonctif: ['sente', 'sentes', 'sente', 'sentions', 'sentiez', 'sentent']
+        }
+    },
+    {
+        infinitif: 'partir',
+        meaning: '离开，出发',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['pars', 'pars', 'part', 'partons', 'partez', 'partent'],
+            passe_compose: ['suis parti', 'es parti', 'est parti', 'sommes partis', 'êtes partis', 'sont partis'],
+            imparfait: ['partais', 'partais', 'partait', 'partions', 'partiez', 'partaient'],
+            futur_simple: ['partirai', 'partiras', 'partira', 'partirons', 'partirez', 'partiront'],
+            conditionnel: ['partirais', 'partirais', 'partirait', 'partirions', 'partiriez', 'partiraient'],
+            subjonctif: ['parte', 'partes', 'parte', 'partions', 'partiez', 'partent']
+        }
+    },
+    {
+        infinitif: 'dormir',
+        meaning: '睡觉',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['dors', 'dors', 'dort', 'dormons', 'dormez', 'dorment'],
+            passe_compose: ['ai dormi', 'as dormi', 'a dormi', 'avons dormi', 'avez dormi', 'ont dormi'],
+            imparfait: ['dormais', 'dormais', 'dormait', 'dormions', 'dormiez', 'dormaient'],
+            futur_simple: ['dormirai', 'dormiras', 'dormira', 'dormirons', 'dormirez', 'dormiront'],
+            conditionnel: ['dormirais', 'dormirais', 'dormirait', 'dormirions', 'dormiriez', 'dormiraient'],
+            subjonctif: ['dorme', 'dormes', 'dorme', 'dormions', 'dormiez', 'dorment']
+        }
+    },
+    {
+        infinitif: 'courir',
+        meaning: '跑',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['cours', 'cours', 'court', 'courons', 'courez', 'courent'],
+            passe_compose: ['ai couru', 'as couru', 'a couru', 'avons couru', 'avez couru', 'ont couru'],
+            imparfait: ['courais', 'courais', 'courait', 'courions', 'couriez', 'couraient'],
+            futur_simple: ['courrai', 'courras', 'courra', 'courrons', 'courrez', 'courront'],
+            conditionnel: ['courrais', 'courrais', 'courrait', 'courrions', 'courriez', 'courraient'],
+            subjonctif: ['coure', 'coures', 'coure', 'courions', 'couriez', 'courent']
+        }
+    },
+    {
+        infinitif: 'mourir',
+        meaning: '死亡',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['meurs', 'meurs', 'meurt', 'mourons', 'mourez', 'meurent'],
+            passe_compose: ['suis mort', 'es mort', 'est mort', 'sommes morts', 'êtes morts', 'sont morts'],
+            imparfait: ['mourais', 'mourais', 'mourait', 'mourions', 'mouriez', 'mouraient'],
+            futur_simple: ['mourrai', 'mourras', 'mourra', 'mourrons', 'mourrez', 'mourront'],
+            conditionnel: ['mourrais', 'mourrais', 'mourrait', 'mourrions', 'mourriez', 'mourraient'],
+            subjonctif: ['meure', 'meures', 'meure', 'mourions', 'mouriez', 'meurent']
+        }
+    },
+    {
+        infinitif: 'offrir',
+        meaning: '提供，给予',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['offre', 'offres', 'offre', 'offrons', 'offrez', 'offrent'],
+            passe_compose: ['ai offert', 'as offert', 'a offert', 'avons offert', 'avez offert', 'ont offert'],
+            imparfait: ['offrais', 'offrais', 'offrait', 'offrions', 'offriez', 'offraient'],
+            futur_simple: ['offrirai', 'offriras', 'offrira', 'offrirons', 'offrirez', 'offriront'],
+            conditionnel: ['offrirais', 'offrirais', 'offrirait', 'offririons', 'offririez', 'offriraient'],
+            subjonctif: ['offre', 'offres', 'offre', 'offrions', 'offriez', 'offrent']
+        }
+    },
+    {
+        infinitif: 'ouvrir',
+        meaning: '打开',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['ouvre', 'ouvres', 'ouvre', 'ouvrons', 'ouvrez', 'ouvrent'],
+            passe_compose: ['ai ouvert', 'as ouvert', 'a ouvert', 'avons ouvert', 'avez ouvert', 'ont ouvert'],
+            imparfait: ['ouvrais', 'ouvrais', 'ouvrait', 'ouvrions', 'ouvriez', 'ouvraient'],
+            futur_simple: ['ouvrirai', 'ouvriras', 'ouvrira', 'ouvrirons', 'ouvrirez', 'ouvriront'],
+            conditionnel: ['ouvrirais', 'ouvrirais', 'ouvrirait', 'ouvririons', 'ouvririez', 'ouvriraient'],
+            subjonctif: ['ouvre', 'ouvres', 'ouvre', 'ouvrions', 'ouvriez', 'ouvrent']
+        }
+    },
+    {
+        infinitif: 'souffrir',
+        meaning: '受苦，遭受',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['souffre', 'souffres', 'souffre', 'souffrons', 'souffrez', 'souffrent'],
+            passe_compose: ['ai souffert', 'as souffert', 'a souffert', 'avons souffert', 'avez souffert', 'ont souffert'],
+            imparfait: ['souffrais', 'souffrais', 'souffrait', 'souffrions', 'souffriez', 'souffraient'],
+            futur_simple: ['souffrirai', 'souffriras', 'souffrira', 'souffrirons', 'souffrirez', 'souffriront'],
+            conditionnel: ['souffrirais', 'souffrirais', 'souffrirait', 'souffririons', 'souffririez', 'souffriraient'],
+            subjonctif: ['souffre', 'souffres', 'souffre', 'souffrions', 'souffriez', 'souffrent']
+        }
+    },
+    {
+        infinitif: 'couvrir',
+        meaning: '覆盖',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['couvre', 'couvres', 'couvre', 'couvrons', 'couvrez', 'couvrent'],
+            passe_compose: ['ai couvert', 'as couvert', 'a couvert', 'avons couvert', 'avez couvert', 'ont couvert'],
+            imparfait: ['couvrais', 'couvrais', 'couvrait', 'couvrions', 'couvriez', 'couvraient'],
+            futur_simple: ['couvrirai', 'couvriras', 'couvrira', 'couvrirons', 'couvrirez', 'couvriront'],
+            conditionnel: ['couvrirais', 'couvrirais', 'couvrirait', 'couvririons', 'couvririez', 'couvriraient'],
+            subjonctif: ['couvre', 'couvres', 'couvre', 'couvrions', 'couvriez', 'couvrent']
+        }
+    },
+    {
+        infinitif: 'cueillir',
+        meaning: '采摘',
+        group: '第二组 (-ir)',
+        conjugations: {
+            present: ['cueille', 'cueilles', 'cueille', 'cueillons', 'cueillez', 'cueillent'],
+            passe_compose: ['ai cueilli', 'as cueilli', 'a cueilli', 'avons cueilli', 'avez cueilli', 'ont cueilli'],
+            imparfait: ['cueillais', 'cueillais', 'cueillait', 'cueillions', 'cueilliez', 'cueillaient'],
+            futur_simple: ['cueillerai', 'cueilleras', 'cueillera', 'cueillerons', 'cueillerez', 'cueilleront'],
+            conditionnel: ['cueillerais', 'cueillerais', 'cueillerait', 'cueillerions', 'cueilleriez', 'cueilleraient'],
+            subjonctif: ['cueille', 'cueilles', 'cueille', 'cueillions', 'cueilliez', 'cueillent']
+        }
+    }
+];
+
+// 导出数据供主程序使用
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { group2Verbs };
+}
